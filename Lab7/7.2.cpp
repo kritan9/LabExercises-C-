5e5e2fbd3d73180d4a43fcc4de3ad9f2d7970cf7
kritan <kritanbanstola1212@gmail.com>
2019-01-23 21:15:35 +0545

    A style

--- Lab7/7.2.cpp
+++ Lab7/7.2.cpp
@@ -8,44 +8,53 @@ protected:
     string name;
     int age;
 public:
-    Person(){}
-    Person(string n,int a):name(n), age(a){}
-    string Name(){
+    Person() {}
+    Person(string n,int a):name(n), age(a) {}
+    string Name()
+    {
         return name;
     }
-   int Age(){
-       return age;
+    int Age()
+    {
+        return age;
     }
 };
-class Employee:virtual public Person{
+class Employee:virtual public Person
+{
 protected:
     float salary;
 public:
-    Employee(string n,int a,float sal):Person(n,a),salary(sal){
-    name=n;}
-    float Salary(){
+    Employee(string n,int a,float sal):Person(n,a),salary(sal)
+    {
+        name=n;
+    }
+    float Salary()
+    {
         return salary;
     }
 };
-class Student:virtual public Person{
+class Student:virtual public Person
+{
 protected:
     string sub;
 public:
-    Student(string n,int a,string subj):Person(n,a),sub(subj){}
-    string Subject(){
+    Student(string n,int a,string subj):Person(n,a),sub(subj) {}
+    string Subject()
+    {
         return sub;
     }
 };
-class Manager:public Employee, public Student{
+class Manager:public Employee, public Student
+{
 public:
-     Manager(string n,int a,string sub,float sal):Person(n,a),Employee(n,a,sal),Student(n,a,sub){}
+    Manager(string n,int a,string sub,float sal):Person(n,a),Employee(n,a,sal),Student(n,a,sub) {}
 };
 int main()
 {
-  Manager Microsoft("Ram Pandey",30,"AI",25000);
-  cout<<setw(10)<<"Name :"<<Microsoft.Name()<<endl;
-  cout<<setw(10)<<"Age :"<<Microsoft.Age()<<endl;
-  cout<<setw(10)<<"Subject :"<<Microsoft.Subject()<<endl;
-  cout<<setw(10)<<"Salary :"<<Microsoft.Salary()<<endl;
-  return 0;
+    Manager Microsoft("Ram Pandey",30,"AI",25000);
+    cout<<setw(10)<<"Name :"<<Microsoft.Name()<<endl;
+    cout<<setw(10)<<"Age :"<<Microsoft.Age()<<endl;
+    cout<<setw(10)<<"Subject :"<<Microsoft.Subject()<<endl;
+    cout<<setw(10)<<"Salary :"<<Microsoft.Salary()<<endl;
+    return 0;
 };
--- Lab7/7.3.cpp
+++ Lab7/7.3.cpp
@@ -7,52 +7,60 @@ protected:
     string name;
     int age;
 public:
-    Student(){}
-    Student(string n,int a):name(n), age(a){}
-    string Name(){
+    Student() {}
+    Student(string n,int a):name(n), age(a) {}
+    string Name()
+    {
         return name;
     }
-   int Age(){
-       return age;
+    int Age()
+    {
+        return age;
     }
     virtual string Subject()=0;
 };
-class Engineering:public Student{
+class Engineering:public Student
+{
 public:
-    Engineering(){}
-    Engineering(string n,int a):Student(n,a){}
-    string Subject(){
+    Engineering() {}
+    Engineering(string n,int a):Student(n,a) {}
+    string Subject()
+    {
         return "Engineering";
     }
 };
-class Medicine:public Student{
+class Medicine:public Student
+{
 public:
-    Medicine(){}
-    Medicine(string n,int a):Student(n,a){}
-    string Subject(){
+    Medicine() {}
+    Medicine(string n,int a):Student(n,a) {}
+    string Subject()
+    {
         return "Medicine";
     }
 };
-class Science:public Student{
+class Science:public Student
+{
 public:
-    Science(){}
-    Science(string n,int a):Student(n,a){}
-    string Subject(){
+    Science() {}
+    Science(string n,int a):Student(n,a) {}
+    string Subject()
+    {
         return "Science";
     }
 };
 int main()
 {
     Student* st[3];
-  st[0] = new Engineering("Kritan",19);
-  st[1] = new Medicine("Hari",20);
-  st[2] = new Science("Shyam",18);
-  cout << "Student of various field" << endl;
-  for (int i = 0; i < 3; ++i)
-  {
-    cout << "Name :" << st[i]->Name() << endl;
-    cout << "Age  :" << st[i]->Age() << endl;
-    cout << "Sub  :" << st[i]->Subject() << endl;
-  }
-  return 0;
+    st[0] = new Engineering("Kritan",19);
+    st[1] = new Medicine("Hari",20);
+    st[2] = new Science("Shyam",18);
+    cout << "Student of various field" << endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << "Name :" << st[i]->Name() << endl;
+        cout << "Age  :" << st[i]->Age() << endl;
+        cout << "Sub  :" << st[i]->Subject() << endl;
+    }
+    return 0;
 };
